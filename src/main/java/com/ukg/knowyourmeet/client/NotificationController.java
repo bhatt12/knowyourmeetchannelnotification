ae56813ed685d25477a11651d755330ad05cc191
Shivam Gupta <shivam.gupta@ukg.com>
2023-07-17 10:41:12 +0530

    updated get notifcation call

--- src/main/java/com/ukg/knowyourmeet/client/NotificationController.java
+++ src/main/java/com/ukg/knowyourmeet/client/NotificationController.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,13 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/jira")
 public class NotificationController {
 
     @GetMapping(value = "/notification", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> getSSOTemplateData(@RequestParam("id") String boardID) throws IOException {
+    public List<Object> getNotificationData(@RequestParam("id") String boardID) throws IOException {
 
         RestTemplate restTemplate = new RestTemplate();
         String url = "https://chatitout.atlassian.net/rest/api/2/search?jql=";
@@ -29,14 +36,32 @@ public class NotificationController {
         HttpEntity<String> entity = new HttpEntity<>(headers);
         ResponseEntity<String> responseEntity =  restTemplate.exchange(url, HttpMethod.GET, entity, new ParameterizedTypeReference<String>(){});
 
-        JSONObject obj = new JSONObject((responseEntity.getBody()));
+        List<Object> dataList = new ArrayList<>();
+        JSONObject responseObj = new JSONObject((responseEntity.getBody()));
+        JSONArray issuesArray = responseObj.getJSONArray("issues");
 
-        JSONArray arr = new JSONArray(obj.get("issues"));
-        System.out.println(arr.get(0));
+        for (int i = 0; i < issuesArray.length(); i++) {
+            JSONObject object = issuesArray.getJSONObject(i);
+            String key = (String) object.get("key");
+            String summary = (String) object.getJSONObject("fields").get("summary");
+            String assignee = (String) object.getJSONObject("fields").getJSONObject("assignee").get("displayName");
+            String status = (String) object.getJSONObject("fields").getJSONObject("status").get("name");
+            String priority = (String) object.getJSONObject("fields").getJSONObject("priority").get("name");
 
-//        list.stream().forEach(i -> {System.out.print(i + " ");});
+            Resource resource = new ClassPathResource("schema.json");
+            try (InputStream inputStream = resource.getInputStream()) {
+                String schemaJson = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+                schemaJson = schemaJson.replace("{{STORY_ID}}",key);
+                schemaJson = schemaJson.replace("{{STORY_DESC}}",summary);
+                schemaJson = schemaJson.replace("{{STORY_ASSIGNEE}}",assignee);
+                schemaJson = schemaJson.replace("{{STORY_STATUS}}",status);
+                dataList.add(schemaJson);
+//                System.out.println(schemaJson);
+            }
+        }
 
-        return responseEntity;
+        System.out.println(dataList);
+        return dataList;
     }
 
 }
